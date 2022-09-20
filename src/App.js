8d7272a00bf1eb7fd0238b09563f431821cc825f
hyeokju <gurwn805@naver.com>
2022-09-20 14:07:56 +0900

    슬라이드 및 카테고리 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from 'react';
 import Banner from './component/Banner.js';
+import MainCarousel from './component/Carousel';
 import Nav from './component/Nav.js';
 
 function App() {
   let [inputValue, setInputValue] = useState("");
+    let navigate = useNavigate();
 
   return (
     <div className="App">
       <Banner inputValue={inputValue} setInputValue={setInputValue} />
-      <Nav />
+      <div className='aa'>
+        <ul>
+        <li onClick={()=> { navigate("/bestbook") }}>베스트셀러</li>
+        <li onClick={()=> { navigate("/book") }}>국내도서</li>
+        <li onClick={()=> { navigate("/book") }}>외국도서</li>
+        <li onClick={()=> { navigate("/music") }}>음반</li>
+        </ul>
+      </div>
+      <MainCarousel />
     </div>
   );
 }
--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,5 +1,4 @@
 import { Link, useNavigate } from "react-router-dom";
-import Carousel from "react-bootstrap/Carousel";
 import food1 from '../images/food1.jpg';
 import './Nav.css';
 
@@ -10,49 +9,12 @@ function Nav() {
 
     return (
         <div>
-            <div>
+            <container className="headerBar">
             <Link onClick={()=> { navigate("/bestbook") }}>베스트셀러</Link>
             <Link onClick={()=> { navigate("/book") }}>국내도서</Link>
-            </div>
-            <Carousel>
-      <Carousel.Item>
-        <img
-          className="carousel-inner"
-          src={food1}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="carousel-inner"
-          src={food1}
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="carousel-inner"
-          src={food1}
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+            <Link onClick={()=> { navigate("/book") }}>외국도서</Link>
+            <Link onClick={()=> { navigate("/music") }}>음반</Link>
+            </container>
         </div>
     );
 }
