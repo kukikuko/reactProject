e4ed287a4e0055fba846a84407bc93e4149b8c70
hyeokju <gurwn805@naver.com>
2022-09-20 11:52:46 +0900

    Banner css 추가

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,15 +1,27 @@
+import { useNavigate } from "react-router-dom";
 import logo from '../images/logo.png';
+import './Banner.css';
+import { FaSearch } from "react-icons/fa";
 
 function Banner({inputValue, setInputValue}) {
+    let navigate = useNavigate();
+
     return(
         <div>
-            <img src={logo} alt="kurlyLogo" className="logo" />
-            <span>마켓컬리</span>
-            <input 
-                type="text" name="value" value={inputValue} placeholder="검색어를 입력해주세요" 
-                onChange={(event) => {
-                    setInputValue(event.target.value);
-                }} />
+            <p style={{textAlign: "cneter"}}>
+                <img src={logo} alt="kurlyLogo" style={{margin : "20px auto", display: "block"}} 
+                    onClick={() => {navigate("/");}}/>
+            </p>
+            <div className="searchBox">
+            <form>
+                <input 
+                    type="text" name="value" value={inputValue} placeholder="검색어를 입력해주세요" 
+                    onChange={(event) => {
+                        setInputValue(event.target.value);
+                    }} />
+                {/* <FaSearch className="searchIcon"/> */}
+            </form>
+            </div>
         </div>
     );
 }
